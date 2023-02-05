475d74e72c03a29f871f30e8d3de31b71de873a6
Sahapat <sahapat.starcaft@gmail.com>
2023-02-05 14:41:05 +0700

    Fix: distribute gitignore by project context

---
+++ nextjs/src/lib/axios.ts
@@ -0,0 +1,43 @@
+import environment from "@/util/environment";
+import { message } from "antd";
+import axios, { AxiosRequestConfig } from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: environment.API_URL,
+});
+
+axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.headers = {
+    ...config.headers,
+    "X-API-Key": environment.API_KEY || "",
+  };
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response: any) => {
+    return response;
+  },
+  (error: any) => {
+    switch (error?.response?.status) {
+      case 401:
+        message.error("Unauthorized! Please try to login again");
+        break;
+      case 403:
+        message.error("Forbidden");
+        break;
+      case 406:
+        message.error("Not Acceptable");
+        break;
+      case 422:
+        message.error("Unprocessable Entity");
+        break;
+      default:
+        message.error(error.message);
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
